427e3b341fe739de39651cef0cf54992bc6e8205
barton <barton.satchwill@gmail.com>
2015-01-23 08:26:08 -0700

    store rendered images to Swift; report status of swift and rendering

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 #!flask/bin/python
 
+import sys, os, shutil, time
+import subprocess, shlex
 from flask import Flask,jsonify,abort,request
-import os, shutil, time
 from threading import Thread
+sys.path.append("/usr/lib/python2.7/dist-packages")
+import swiftclient
 
 app = Flask(__name__)
 
@@ -14,12 +17,12 @@ blend_root = '/home/ubuntu/blend'
 
 @app.route('/set_repo', methods=['POST', 'GET'])
 def set_repo():
-	blend_dir = '/home/ubuntu/blend'
-	if os.path.isdir(blend_dir):
-		shutil.rmtree(blend_dir)
-	cmd = 'git clone %s %s' % (request.args.get('url'), blend_dir)
+	if os.path.isdir(blend_root):
+		shutil.rmtree(blend_root)
+	cmd = 'git clone %s %s' % (request.args.get('url'), blend_root)
 	os.system(cmd)
 	return ""
+
 
 
 @app.route('/render_start', methods=['POST','GET'])
@@ -37,14 +40,25 @@ def render_start():
 	return ''
 
 
+
 @app.route('/render_stop', methods=['POST','GET'])
 def render_stop():
-	return print_args(request.args)
+	return subprocess.check_output(['pkill','blender'])
+
 
 
 @app.route('/render_status', methods=['POST','GET'])
 def render_status():
-	return print_args(request.args)
+	hostname = subprocess.check_output('hostname')[:-1]
+	render_status = '================= ' + hostname + ' ====================\n'
+	cmd = shlex.split('grep "Saved" /home/ubuntu/render.log')
+	p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+	for item in p.communicate():
+		print item
+		render_status = render_status + item + '\n'
+	return render_status
+
 
 
 app.route('/')
@@ -52,12 +66,14 @@ def index():
 	return "Hi, Bart!\n"
 
 
+# ==================================================================================
 
 def print_args(args):
-        arg_list = []
-        for arg in args:
-                arg_list.append(arg)
-        return "returning " + ", ".join(arg_list)
+	arg_list = []
+	for arg in args:
+		arg_list.append(arg)
+	return "returning " + ", ".join(arg_list)
+
 
 
 def render(blender, blend, output_dir, start_frame, end_frame, logfile):
@@ -71,16 +87,52 @@ def render(blender, blend, output_dir, start_frame, end_frame, logfile):
 	f.close
 	os.system(cmd)
 	store()
+	swift_status()
+
+
+
+def swift_status():
+	root = '/home/ubuntu'
+	bucket = 'mybucket'
+
+	print "reporting on Swift..."
+	swift = swiftclient.Connection(
+	authurl=os.environ['OS_AUTH_URL'], key=os.environ['OS_PASSWORD'],
+	user=os.environ['OS_USERNAME'], tenant_name=os.environ['OS_TENANT_NAME'],
+	auth_version=2)
+
+	print "----------------"
+	for container in swift.get_account()[1]:
+		#if bucket is container['name']:
+		if True:
+			print container['name']
+			for data in swift.get_container(container['name'])[1]:
+				print '{0}\t{1}\t{2}'.format(data['name'], data['bytes'], data['last_modified'])
+			print "----------------"
+
 
 
 def store():
-	print "uploading to Swift..."
+	root = '/home/ubuntu'
+	bucket = 'mybucket'
 
+	print "uploading to Swift..."
+	swift = swiftclient.Connection(
+		authurl=os.environ['OS_AUTH_URL'],
+		key=os.environ['OS_PASSWORD'],
+		user=os.environ['OS_USERNAME'],
+		tenant_name=os.environ['OS_TENANT_NAME'],
+		auth_version=2)
+
+	swift.put_container(bucket)
+	for dirname, subdirs, filenames in os.walk(os.path.join(root, 'images')):
+		for filename in filenames:
+			f = os.path.join(dirname, filename).replace(root, '').lstrip('/')
+			swift.put_object(bucket, f, open(f,'r'))
 
 
 
 
 if __name__ == '__main__':
-	#app.run(debug=True)
-	#app.run(host='0.0.0.0', port=port, debug=True)
 	app.run(host='::', debug=True)
+
